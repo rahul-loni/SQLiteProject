a63c1bd729e8658a609b4d72263244cca1edd911
Rahul Kumar <rahulismt491@gamail.com>
2025-02-07 08:56:06 +0545

    second commit

--- app/src/main/java/com/example/sqlite_project1/DatabaseHelper.java
+++ app/src/main/java/com/example/sqlite_project1/DatabaseHelper.java
@@ -1,6 +1,65 @@
 package com.example.sqlite_project1;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class SQLiteHelper extends SQLiteOpenHelper {
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME="student.db";
+    public static final String TABLE_NAME="student_table";
+    public static final String COL1="id";
+    public static final String COL2="name";
+    public static final String COL3="marks";
+//    public static final int versionname=1;
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+      sqLiteDatabase.execSQL(" create table " + TABLE_NAME +
+               "(ID INTEGER PRIMARY KEY AUTOINCREMENT ,"+
+              "NAME TEXT , MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+    //Data add or insert method
+    public boolean insertData(String id,String name,String marks){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL1,id);
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,marks);
+        Long result=db.insert(TABLE_NAME,null,contentValues);
+        if (result==1){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
 }
+
+////    Data insert Method
+//   public  boolean insertData(String id,String name,String marks){
+//      SQLiteDatabase db=this.getWritableDatabase();
+//      ContentValues contentValues=new ContentValues();
+//      contentValues.put(COL1,id);
+//      contentValues.put(COL2,name);
+//      contentValues.put(COL3,marks);
+//     Long result=db.insert(TABLE_NAME,null,contentValues);
+//     if (result==1){
+//      return false;
+//     }else {
+//         return true;
+//     }
+//
+//   }
+
--- app/src/main/java/com/example/sqlite_project1/MainActivity.java
+++ app/src/main/java/com/example/sqlite_project1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sqlite_project1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText id,name,marks;
+    Button insert,view ,edit,delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        id=findViewById(R.id.txt_id);
+        name=findViewById(R.id.txt_name);
+        marks=findViewById(R.id.txt_marks);
+        insert=findViewById(R.id.btn_view);
+        view=findViewById(R.id.btn_view);
+        edit=findViewById(R.id.btn_Edit);
+        delete=findViewById(R.id.btn_delete);
+
+
     }
 }
